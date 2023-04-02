e71dbcb32f3bb8be140132cf1cf59f470aed5db8
jorge <jorselcer@alu.edu.gva.es>
2023-04-02 18:49:05 +0200

    tests realizados

--- src/TestCoche.java
+++ src/TestCoche.java
@@ -1,17 +1,45 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestCoche {
 
+	
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_crear_coche() {
+		Coche nuevoCoche = new Coche();
 	}
+	@Test
+	void test_al_crear_un_coche_su_velocidad_es_cero() {
+		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0,nuevoCoche.velocidad);
+	}
+	@Test
+	void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.acelerar(30);
+		Assertions.assertEquals(30,nuevoCoche.velocidad);
+	}
+	@Test
+	void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.velocidad = 50;
+		nuevoCoche.decelerar(20);
+		Assertions.assertEquals(30,nuevoCoche.velocidad);
+	}
+	@Test
+	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.velocidad = 50;
+		nuevoCoche.decelerar(80);
+		Assertions.assertEquals(0,nuevoCoche.velocidad);
+	}
+
 
 }
