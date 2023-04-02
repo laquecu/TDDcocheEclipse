0dac49f8e7de41f8510d5d056c2f8f0548e4040d
jorge <jorselcer@alu.edu.gva.es>
2023-04-02 19:28:49 +0200

    añadir mi nombre a los métodos

--- src/Coche.java
+++ src/Coche.java
@@ -3,12 +3,12 @@ public class Coche {
 
 	public int velocidad;
 
-	public void acelerar(int aceleracion) {
+	public void acelerar_JorgeSelva(int aceleracion) {
 		velocidad += aceleracion;
 		
 	}
 
-	public void decelerar(int deceleracion) {
+	public void decelerar_JorgeSelva(int deceleracion) {
 		velocidad -= deceleracion;
 		if(velocidad < 0) velocidad = 0;
 		
--- src/TestCoche.java
+++ src/TestCoche.java
@@ -12,32 +12,32 @@ class TestCoche {
 	}
 
 	@Test
-	void test_crear_coche() {
+	void test_crear_coche_JorgeSelva() {
 		Coche nuevoCoche = new Coche();
 	}
 	@Test
-	void test_al_crear_un_coche_su_velocidad_es_cero() {
+	void test_al_crear_un_coche_su_velocidad_es_cero_JorgeSelva() {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0,nuevoCoche.velocidad);
 	}
 	@Test
-	void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+	void test_al_acelerar_un_coche_su_velocidad_aumenta_JorgeSelva() {
 		Coche nuevoCoche = new Coche();
-		nuevoCoche.acelerar(30);
+		nuevoCoche.acelerar_JorgeSelva(30);
 		Assertions.assertEquals(30,nuevoCoche.velocidad);
 	}
 	@Test
-	void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+	void test_al_decelerar_un_coche_su_velocidad_disminuye_JorgeSelva() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad = 50;
-		nuevoCoche.decelerar(20);
+		nuevoCoche.decelerar_JorgeSelva(20);
 		Assertions.assertEquals(30,nuevoCoche.velocidad);
 	}
 	@Test
-	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero() {
+	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_JorgeSelva() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad = 50;
-		nuevoCoche.decelerar(80);
+		nuevoCoche.decelerar_JorgeSelva(80);
 		Assertions.assertEquals(0,nuevoCoche.velocidad);
 	}
 
